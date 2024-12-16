640d70c017fc811ff170adf640d11ce88d6585e1
gbaorăẫâô <trangiabao@gmail.com>
2024-12-16 13:28:21 +0700

    Cập nhật các file và sửa lỗi

--- client/src/CommentSection.js
+++ client/src/CommentSection.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './asests/css/CommentSection.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function CommentSection({ productId }) {
     const [comments, setComments] = useState([]);
--- client/src/EditProduct.js
+++ client/src/EditProduct.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const EditProduct = () => {
   const { id } = useParams();
