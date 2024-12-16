cb72fd37c5865f5688e8f6a16848edc67e9836e8
gbaorăẫâô <trangiabao@gmail.com>
2024-12-16 15:17:04 +0700

    Mô tả ngắn gọn về thay đổi

--- client/src/TopBrandCarousel.js
+++ client/src/TopBrandCarousel.js
@@ -1,6 +1,7 @@
 import React from "react";
-
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./asests/css/TopBrandsCarousel.css"; // Adjust path as needed
 
 const TopBrandsCarousel = () => {
